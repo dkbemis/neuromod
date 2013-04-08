13cc47b5c44644d43ea3a254ed6ec05d74eb6478
dkbemis <doug.bemis@gmail.com>
2013-04-09 00:12:10 +0200

    MEEG sanity check

--- analysis/NM_SanityCheckMEEGData.m
+++ analysis/NM_SanityCheckMEEGData.m
@@ -11,15 +11,21 @@ end
 global GLA_subject;
 global GLA_meeg_type;
 disp(['Sanity checking ' GLA_meeg_type ' data for ' GLA_subject '...']);
+NM_LoadSubjectData({...
+    {[GLA_meeg_type '_blinks_data_preprocessed'],1},...
+    {[GLA_meeg_type '_word_5_data_preprocessed'],1},...
+    });
 
-% Plot the averages for the final word
-global GLA_meeg_trial_type;
-GLA_meeg_trial_type = 'word_5';
+% Plot the averages for the blinks and the final word
+global GLA_trial_type;
+types = {'blinks','word_5'};
+for t = 1:length(types)
+    GLA_trial_type = types{t};
 
-% Save into the analysis folder
-curr_dir = pwd;
-cd([NM_GetCurrentDataDirectory() '/analysis/' GLA_subject ]);
-NM_DisplayMEEGAverages([GLA_subject '_sanity_check']);
-cd(curr_dir);
-
+    % Save into the analysis folder
+    curr_dir = pwd;
+    cd([NM_GetCurrentDataDirectory() '/analysis/' GLA_subject ]);
+    NM_DisplayMEEGAverages([GLA_subject '_' types{t} '_' GLA_meeg_type '_averages']);
+    cd(curr_dir);
+end
 
